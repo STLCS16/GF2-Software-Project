4b7c7051152031b5a2de0eab8835747a01784780
Grace Chen <rc956@cam.ac.uk>
2026-05-26 18:30:26 +0100

    scanner Grace version

--- logsim/scanner.py
+++ logsim/scanner.py
@@ -25,7 +25,7 @@ class Symbol:
 
     def __init__(self):
         """Initialise symbol properties."""
-        self.type = None
+        self.type = None 
         self.id = None
         self.line = None 
         self.column = None 
@@ -50,26 +50,47 @@ class Scanner:
                       and returns the symbol.
     """
 
+    # type ID
+    (HEADINGS, END, DEVICE, IDENTIFIER, NAME, COMMA, SEMICOLON, EQUAL, COLON, DOT, OPEN_PAREN, CLOSE_PAREN, ARROW, NUMBER,EOF ) = range(15)
+    
     def __init__(self, path, names):
         self.names = names
         with open(path, 'r') as f:
-            self.file_lines = f.readlines()
+            self.source_string = f.read()
         
-        self.line_number = 0
-        self.column_number = 0
-        self.current_character = " " 
-    
-        self.symbol_type_list = [
-            self.COMMA, self.SEMICOLON, self.EQUALS, self.COLON, self.DOT,
-            self.OPEN_PAREN, self.CLOSE_PAREN, self.ARROW,
-            self.KEYWORD, self.NUMBER, self.NAME, self.EOF
-        ] = range(12)
-    
-        self.keywords_list = ["DEVICES", "CONNECTIONS", "SIGNALS", "END"]
-        self.device_list = []
-        self.keyword_ids = self.names.lookup(self.keywords_list)
-    
-        self.advance()
+        self.char_index = 0
+        self.line_number = 1
+        self.column_number = 1
+        self.current_character = self.source_string[0] if self.source_string else ""
+        # heading
+        self.heading_list = ["DEVICES", "CONNECTIONS", "SIGNALS"]
+        self.names.lookup(self.heading_list) # pre-regist
+        #end
+        self.end_list = 'END'
+        self.names.lookup(self.end_list)
+        #device
+        self.device_list_ni = ["DTYPE", "XOR","NOT"]#ni means no need to specify number of input
+        self.device_list_i = ["AND", "OR", "NAND", "NOR","CLOCK", "SWITCH"] # i means need to specify number of input
+        self.names.lookup(self.device_list_ni + self.device_list_i)
+        #identifier
+        self.identifier_list = [f'I{i}' for i in range(1, 17)] + ['DATA', 'CLK', 'SET','CLEAR', 'Q','QBAR']
+        self.names.lookup(self.identifier_list) # avoid user name the device as I1 or other confusing term
+        #punctuation
+        self.comma = ','
+        self.semicolon =';'
+        self.equal ='='
+        self.colon = ':'
+        self.dot ='.'
+        self.open_paren ='('
+        self.close_paren = ')'
+        self.minus = '-'
+        self.greater = '>'
+        self.newline = '\n'
+
+        #fast search
+        self.heading_set = set(self.heading_list)
+        self.device_set = set(self.device_list_ni + self.device_list_i)
+        self.identifier_set = set(self.identifier_list)
 
     def get_symbol(self):
         """Translate the next sequence of characters into a symbol."""
@@ -79,16 +100,34 @@ class Scanner:
         symbol.line = self.line_number
         symbol.column = self.column_number
 
-        if self.current_character.isalpha(): # name
-            name_string = self.get_name()
-            if name_string in self.keywords_list:
-                symbol.type = self.KEYWORD
+        if self.current_character == "":
+            symbol.type = self.EOF
+            return symbol
+
+        if self.current_character.isalpha(): 
+            name_list = self.get_name()
+            name_string = name_list[0]
+            symbol.id = self.names.lookup([name_string])[0]
+            if self.name_string.islower():
+                return None
+            if name_string in self.heading_set:
+                symbol.type = self.HEADINGS
+            elif name_string in self.end_list:
+                symbol.type = self.END
+            elif name_string in self.device_set:
+                    symbol.type = self.DEVICE
+            elif name_string in self.identifier_list:
+                    symbol.type = self.IDENTIFIER
             else:
                 symbol.type = self.NAME
-            [symbol.id] = self.names.lookup([name_string])
-        elif self.current_character.isdigit(): # number
+            return symbol
+                
+        if self.current_character.isdigit(): 
             symbol.id = self.get_number()
             symbol.type = self.NUMBER
+            return symbol
+        
+        #punctuation
         elif self.current_character == ";":
             symbol.type = self.SEMICOLON
             self.advance()
@@ -110,7 +149,6 @@ class Scanner:
         elif self.current_character == ",":
             symbol.type = self.COMMA
             self.advance()
-        
         elif self.current_character == "-":
             self.advance()
             if self.current_character == ">":
@@ -119,11 +157,41 @@ class Scanner:
             else:
                 symbol.type = self.EOF # Or handle as invalid character
             
-        elif self.current_character == "":
-            symbol.type = self.EOF
         else:
-        # Invalid character handling
             self.advance()
+            return None
         
         return symbol
 
+    def get_name(self):
+        """Builds a string of alphanumeric characters."""
+        name_chars = []
+        while self.current_character.isalnum():
+            name_chars.append(self.current_character)
+            self.advance()
+        return "".join(name_chars)
+
+    def get_number(self):
+        """Builds a string of continuous digits."""
+        num_chars = []
+        while self.current_character.isdigit():
+            num_chars.append(self.current_character)
+            self.advance()
+        return "".join(num_chars)
+
+    def skip_spaces(self):
+        while self.current_character.isspace():
+            self.advance()
+
+    def advance(self):
+        self.char_index += 1
+        if self.char_index < len(self.source_string):
+            self.current_character = self.source_string[self.char_index]
+            if self.current_character == '\n':
+                self.line_number += 1
+                self.column_number = 1
+            else:
+                self.column_number += 1
+        else:
+            self.current_character = ""  # EOF reached
+        return self.current_character
