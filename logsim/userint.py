fe027c8b0620340cfe84f5a41e53329033cecd29
Elvira Munteanu <em952@cam.ac.uk>
2026-06-09 11:00:38 +0100

    Update RC and SIGGEN tests

--- logsim/userint.py
+++ logsim/userint.py
@@ -253,7 +253,6 @@ class UserInterface:
             if self.network.execute_network(cycle_num): #added parameter cycles
                 self.monitors.record_signals()
             else:
-                print("checkpoint 0")
                 print("Error! Network oscillating.")
                 return False
         self.monitors.display_signals()
